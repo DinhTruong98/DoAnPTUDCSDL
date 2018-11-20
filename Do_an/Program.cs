002a8d434c85ac81baae2380ecf1dd017972699f
Trương Công Định <congdinh.truong98@gmail.com>
2018-11-20 22:29:51 +0700

    Sua lai file Program
    sua lai file program, thay form bat dau bang form login

--- Do_an/Program.cs
+++ Do_an/Program.cs
@@ -16,7 +16,7 @@ namespace Do_an
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
