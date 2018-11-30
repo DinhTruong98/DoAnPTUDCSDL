f613451e6aa18ef8a533412f027aa51eebb1d32e
Cong Dinh <congdinh.truong98@gmail.com>
2018-12-01 00:21:18 +0700

    update

---
+++ Do_an/provider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Do_an
+{
+    public static class provider
+    {
+        static public int conn;
+
+        static public void executeQauery(conn)
+        {
+        }
+    }
+}
